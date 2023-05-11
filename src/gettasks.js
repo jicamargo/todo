c5e7c853f3d80dc52f8feb97c4430887b672d21f
jic <ji_camargo@yahoo.com>
2023-05-11 10:02:32 -0500

    delete commented old code  in function gettasks.js

--- src/gettasks.js
+++ src/gettasks.js
@@ -2,35 +2,3 @@ const getTasks = () => JSON.parse(localStorage.getItem('todoData')) || [];
 
 export default getTasks;
 
-// const arrTasks = [
-//   {
-//     description: 'Clean the car',
-//     completed: false,
-//     index: 0,
-//   },
-//   {
-//     description: 'Call mom',
-//     completed: false,
-//     index: 0,
-//   },
-//   {
-//     description: 'Review Microverse tasks',
-//     completed: true,
-//     index: 0,
-//   },
-//   {
-//     description: 'Pay the rent',
-//     completed: false,
-//     index: 0,
-//   },
-//   {
-//     description: 'Stydy for the test',
-//     completed: false,
-//     index: 0,
-//   },
-//   {
-//     description: 'go to the gym',
-//     completed: true,
-//     index: 0,
-//   },
-// ];
