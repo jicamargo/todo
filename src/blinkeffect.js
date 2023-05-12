53fc7bde645463ab766f2e15cee165d486a032df
jic <ji_camargo@yahoo.com>
2023-05-12 12:08:41 -0500

    Delete commented line from blinkeffect.js

--- src/blinkeffect.js
+++ src/blinkeffect.js
@@ -2,7 +2,6 @@ const blinkEffect = (element) => {
   let count = 0;
   const interval = setInterval(() => {
     if (count % 2 === 0) {
-      // element.style.backgroundColor = "yellow";
       element.style.boxShadow = 'inset 0 0 15px #88ccff';
     } else {
       element.style.backgroundColor = '';
